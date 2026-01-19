86d7739d1e7443cacb914707e2d25bf6bd4ef08d
prathik-ds <prathikdsuvarna@gmail.com>
2026-01-19 17:32:04 +0530

    fixed error

--- App.tsx
+++ App.tsx
@@ -52,7 +52,7 @@ const App: React.FC = () => {
             <Footer />
           </div>
         </div>
-        )}
+        <Splash onComplete={handleSplashComplete} />
       </div>
     </Router>
   );
